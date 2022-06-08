12542d975c185ffa95b13794e6f1f3db40a09f9a
Jesus Cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-06-08 18:01:31 -0500

    App pages

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'services.apps.ServicesConfig',
     'blog',
     'social.apps.SocialConfig',
+    'pages.apps.PagesConfig',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Ckeditor
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink']
+        ]
+    }
+}
